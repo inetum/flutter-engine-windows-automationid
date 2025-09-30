a4c106ec4ec1c99aa86ba7b3c061f6ae54fddec0
Loïc PÉRON <loic.peron@inetum.com>
2025-09-30 08:38:44 +0200

    Fix GitHub.hub install

--- setup.bat
+++ setup.bat
@@ -63,4 +63,11 @@ if not defined VSPATH (
     echo Visual Studio with %VS_WORKLOAD_TEXT% workload is already installed at %VSPATH%.
 )
 
-winget install --source wingrt --id=GitHub.hub
+echo Ensure GitHub CLI is available
+for /f "usebackq tokens=*" %%i in (`where hub`) do set "WHERE_HUB=%%i"
+if not defined WHERE_HUB (
+    echo Installing GitHub CLI...
+    call "%WINGET%" install --source winget "GitHub.hub"
+) else (
+    echo GitHub CLI found at %WHERE_HUB%
+)
